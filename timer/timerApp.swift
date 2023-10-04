46c17d4d4248b532dd3f99bf41dc21b7fb446eea
aarish21 <aarish.jerry@gmail.com>
2023-10-04 14:07:27 +0530

    Initial Commit

---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by Aarish on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
